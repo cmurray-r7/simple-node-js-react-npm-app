83b37c8079bb5c0da1cf722ba481c093b15d6f37
Conor Murray <75254135+cmurray-r7@users.noreply.github.com>
2023-03-28 16:20:21 +0100

    Adjust indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "object-curly-spacing": ["warning", "always"],
-        "jsx-quotes": ["warning", "prefer-double"]
-    },
-    "settings": {
-        "react": {
-          "version": "detect"
-        }
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "overrides": [
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "object-curly-spacing": ["warn", "always"],
+    "jsx-quotes": ["warn", "prefer-double"],
+    "indent": ["error", 2]
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
     }
+  }
 }
