bc3920da35797bd4f99ffb3b30d19fe1cea45867
Conor Murray <75254135+cmurray-r7@users.noreply.github.com>
2023-04-06 11:56:41 +0100

    Handle branch numbers in version bump

--- ci/build.sh
+++ ci/build.sh
@@ -2,10 +2,14 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+TICKET_NUMBER="${BRANCH:0:2}"
+REGEX='^[0-9]+$'
+
 if [ "$BRANCH" = "main" ]
 then 
   VERSION=$(npm version minor)
-else
+elif [[ $TICKET_NUMBER =~ $REGEX ]]
+then 
   VERSION=$(npm version patch)
 fi
 
